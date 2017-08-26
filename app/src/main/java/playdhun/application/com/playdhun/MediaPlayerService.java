82ad2414343609b4b6e127b95203c558c52b1c50
suvam <suvam@motorola.com>
2017-08-26 05:32:10 +0530

    Modifying MediaPlayerService.java

--- app/src/main/java/playdhun/application/com/playdhun/MediaPlayerService.java
+++ app/src/main/java/playdhun/application/com/playdhun/MediaPlayerService.java
@@ -4,6 +4,7 @@ import android.app.Service;
 import android.content.Intent;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
+import android.os.Binder;
 import android.os.IBinder;
 import android.provider.MediaStore;
 import android.support.annotation.Nullable;
@@ -49,5 +50,10 @@ public class MediaPlayerService extends Service implements MediaPlayer.OnBufferi
     @Override
     public void onSeekComplete(MediaPlayer mediaPlayer) {
 
+    }
+    public class LocalBinder extends Binder {
+        public MediaPlayerService getService() {
+            return MediaPlayerService.this;
+        }
     }
 }
