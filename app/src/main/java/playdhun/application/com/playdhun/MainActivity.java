29550d65b0d9df81c0c9419604453f98fc41428e
suvam <suvam@motorola.com>
2017-08-28 19:02:20 +0530

    Changing the MediaPlayerServide

--- app/src/main/java/playdhun/application/com/playdhun/MainActivity.java
+++ app/src/main/java/playdhun/application/com/playdhun/MainActivity.java
@@ -1,13 +1,50 @@
 package playdhun.application.com.playdhun;
 
+import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+
+    private MediaPlayerService player;
+    boolean serviceBound = false;
+
+    private ServiceConnection serviceConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
+            MediaPlayerService.LocalBinder binder = (MediaPlayerService.LocalBinder) iBinder;
+            player = binder.getService();
+            serviceBound = true;
+            Toast.makeText(MainActivity.this, "Service Bound", Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName componentName) {
+            serviceBound = false;
+        }
+    };
+    private void playAudio(String media) {
+        if(!serviceBound){
+            Intent playerIntent = new Intent(this, MediaPlayerService.class);
+            playerIntent.putExtra("media", media);
+            startService(playerIntent);
+            bindService(playerIntent, serviceConnection, Context.BIND_AUTO_CREATE);
+        }
+        else{
+
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        playAudio("https://upload.wikimedia.org/wikipedia/commons/6/6c/Grieg_Lyric_Pieces_Kobold.ogg");
     }
 }
--- app/src/main/java/playdhun/application/com/playdhun/MediaPlayerService.java
+++ app/src/main/java/playdhun/application/com/playdhun/MediaPlayerService.java
@@ -1,30 +1,140 @@
 package playdhun.application.com.playdhun;
 
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.Binder;
 import android.os.IBinder;
-import android.provider.MediaStore;
-import android.support.annotation.Nullable;
+import android.util.Log;
+
+import java.io.IOException;
 
 /**
  * Created by Suvam on 8/26/2017.
  */
 
-public class MediaPlayerService extends Service implements MediaPlayer.OnBufferingUpdateListener, MediaPlayer.OnErrorListener, MediaPlayer.OnInfoListener, MediaPlayer.OnPreparedListener, MediaPlayer.OnSeekCompleteListener, AudioManager.OnAudioFocusChangeListener {
+public class MediaPlayerService extends Service implements MediaPlayer.OnCompletionListener, MediaPlayer.OnBufferingUpdateListener, MediaPlayer.OnErrorListener, MediaPlayer.OnInfoListener, MediaPlayer.OnPreparedListener, MediaPlayer.OnSeekCompleteListener, AudioManager.OnAudioFocusChangeListener {
 
     private final IBinder mBinder = new LocalBinder();
-    @Nullable
+    private MediaPlayer mediaPlayer;
+    private String mediaFile;
+    //Used to pause/resume MediaPlayer
+    private int resumePosition;
+    private AudioManager audioManager;
+
+    private void initMediaPlayer(){
+        mediaPlayer = new MediaPlayer();
+        mediaPlayer.setOnCompletionListener(this);
+        mediaPlayer.setOnInfoListener(this);
+        mediaPlayer.setOnBufferingUpdateListener(this);
+        mediaPlayer.setOnPreparedListener(this);
+        mediaPlayer.setOnSeekCompleteListener(this);
+        mediaPlayer.setOnErrorListener(this);
+        //Reseting to ensure that the media player is not pointing to any other data source
+        mediaPlayer.reset();
+
+        mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+        try {
+            mediaPlayer.setDataSource(mediaFile);
+        } catch(IOException e){
+            e.printStackTrace();
+            stopSelf();
+        }
+        mediaPlayer.prepareAsync();
+    }
+
+    private void playMedia(){
+        if(!mediaPlayer.isPlaying()){
+            mediaPlayer.start();
+        }
+    }
+    private void stopMedia() {
+        if(mediaPlayer != null && mediaPlayer.isPlaying()){
+            mediaPlayer.stop();
+        }
+    }
+    private void pauseMedia(){
+        if(mediaPlayer.isPlaying()){
+            mediaPlayer.pause();
+            resumePosition = mediaPlayer.getCurrentPosition();
+        }
+    }
+    private void resumeMedia(){
+        if(!mediaPlayer.isPlaying()){
+            mediaPlayer.seekTo(resumePosition);
+            mediaPlayer.start();
+        }
+    }
+
     @Override
     public IBinder onBind(Intent intent) {
         return null;
     }
 
+    /*
+     * For a good user experience with audio in Android, you need to be careful that your app plays
+     * nicely with the system and other apps that also play media.
+     * To ensure this good user experience the MediaPlayerService will have to handle AudioFocus
+     * events and these are handled in onAudioFocusChange().
+     * This method is a switch statement with the focus events as its cases.
+     * Keep in mind that this override method is called after a request for AudioFocus has been made
+     * from the system or another media app.
+     */
     @Override
-    public void onAudioFocusChange(int i) {
+    public void onAudioFocusChange(int focusState) {
+        switch(focusState){
+            case AudioManager.AUDIOFOCUS_GAIN:
+                //resume playback
+                if(mediaPlayer == null)
+                    initMediaPlayer();
+                else if(!mediaPlayer.isPlaying())
+                    mediaPlayer.start();
+                mediaPlayer.setVolume(1.0f, 1.0f);
+                break;
+            case AudioManager.AUDIOFOCUS_LOSS:
+                //stop playback
+                if(mediaPlayer.isPlaying())
+                    mediaPlayer.stop();
+                mediaPlayer.release();
+                mediaPlayer = null;
+                break;
+            case AudioManager.AUDIOFOCUS_LOSS_TRANSIENT:
+                // Lost focus for a short time, but we have to stop
+                // playback. We don't release the media player because playback
+                // is likely to resume
+                if(mediaPlayer.isPlaying())
+                    mediaPlayer.pause();
+                break;
+            case AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK:
+                // Lost focus for a short time, but it's ok to keep playing
+                // at an low level
+                if(mediaPlayer.isPlaying())
+                    mediaPlayer.setVolume(0.1f, 0.1f);
+                break;
+        }
+    }
 
+    private boolean requestAudioFocus() {
+        audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+        int result = audioManager.requestAudioFocus(this, AudioManager.STREAM_MUSIC, AudioManager.AUDIOFOCUS_GAIN);
+        if(result == AudioManager.AUDIOFOCUS_REQUEST_GRANTED){
+            return true;
+        }
+        return false;
+    }
+
+    private boolean removeAudioFocus() {
+        return AudioManager.AUDIOFOCUS_REQUEST_GRANTED ==
+                audioManager.abandonAudioFocus(this);
+    }
+    @Override
+    public void onCompletion(MediaPlayer mp) {
+        //Called when playback of a media soucrce is completed
+        stopMedia();
+        //now, we need to stop the service
+        stopSelf();
     }
 
     @Override
@@ -33,7 +143,19 @@ public class MediaPlayerService extends Service implements MediaPlayer.OnBufferi
     }
 
     @Override
-    public boolean onError(MediaPlayer mediaPlayer, int i, int i1) {
+    public boolean onError(MediaPlayer mediaPlayer, int whatError, int extra) {
+        //Called when an error occured during the asynchronous operation
+        switch (whatError) {
+            case MediaPlayer.MEDIA_ERROR_NOT_VALID_FOR_PROGRESSIVE_PLAYBACK:
+                Log.d("MediaPlayer Error", "MEDIA ERROR NOT VALID FOR PROGRESSIVE PLAYBACK " + extra);
+                break;
+            case MediaPlayer.MEDIA_ERROR_SERVER_DIED:
+                Log.d("MediaPlayer Error", "MEDIA ERROR SERVER DIED " + extra);
+                break;
+            case MediaPlayer.MEDIA_ERROR_UNKNOWN:
+                Log.d("MediaPlayer Error", "MEDIA ERROR UNKNOWN " + extra);
+                break;
+        }
         return false;
     }
 
@@ -44,13 +166,40 @@ public class MediaPlayerService extends Service implements MediaPlayer.OnBufferi
 
     @Override
     public void onPrepared(MediaPlayer mediaPlayer) {
-
+        //Invoked when the media file is ready for playback
+        playMedia();
     }
 
     @Override
     public void onSeekComplete(MediaPlayer mediaPlayer) {
 
     }
+
+    //The system calls this method when an activity, requests the service be started
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        try{
+            mediaFile = intent.getExtras().getString("media");
+        } catch(NullPointerException e){
+            stopSelf();
+        }
+        if(requestAudioFocus() == false)
+            stopSelf();
+        if(mediaFile != null && mediaFile != "")
+            initMediaPlayer();
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        if(mediaPlayer != null){
+            stopMedia();
+            mediaPlayer.release();
+        }
+        removeAudioFocus();
+    }
+
     public class LocalBinder extends Binder {
         public MediaPlayerService getService() {
             return MediaPlayerService.this;
